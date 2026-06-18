8f0b04b5f05c337c6b5eb9babba13ad6bf5a8091
vanty27 <v.antonyuk95@gmail.com>
2026-06-18 15:25:08 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# My Jobs Agent online
+
+První online verze pro GitHub + Vercel + Supabase.
+
+Soubory:
+- index.html
+- style.css
+- app.js
+- supabase-config.js
+
+Admin email:
+v.antonyuk95@gmail.com
---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://dmwdmdgtsemppdgqfrzz.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_orW_biA272VWR0RHHq_whA_7IxVPKI8";
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
